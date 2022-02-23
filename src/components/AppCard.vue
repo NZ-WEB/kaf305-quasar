82ba5dca27a856c4244187122f1c2f905aba3710
NZ-WEB <stereo00@yandex.ru>
2022-02-23 16:54:38 +0300

    Added card component

---
+++ src/components/AppCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-card class="q-pa-md bg-white" flat>
+    <slot></slot>
+  </q-card>
+</template>
+
+<script>
+export default {
+  // name: 'ComponentName',
+  setup() {
+    return {};
+  },
+};
+</script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -17,45 +17,7 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" show-if-above>
-      <q-card class="my-card" flat bordered>
-        <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" />
-
-        <q-card-section>
-          <div class="text-overline text-orange-9">Overline</div>
-          <div class="text-h5 q-mt-sm q-mb-xs">Title</div>
-          <div class="text-caption text-grey">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </div>
-        </q-card-section>
-
-        <q-card-actions>
-          <q-btn flat color="dark" label="Share" />
-          <q-btn flat color="primary" label="Book" />
-
-          <q-space />
-
-          <q-btn
-            color="grey"
-            round
-            flat
-            dense
-            :icon="expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
-            @click="expanded = !expanded"
-          />
-        </q-card-actions>
-
-        <q-slide-transition>
-          <div v-show="expanded">
-            <q-separator />
-            <q-card-section class="text-subitle2">
-              {{ lorem }}
-            </q-card-section>
-          </div>
-        </q-slide-transition>
-      </q-card>
-    </q-drawer>
+    <the-drawer :leftDrawerOpen="leftDrawerOpen" />
 
     <q-page-container>
       <router-view />
@@ -64,72 +26,23 @@
 </template>
 
 <script lang="ts">
-import EssentialLink from 'components/EssentialLink.vue';
-
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev',
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework',
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev',
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev',
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev',
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev',
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev',
-  },
-];
-
 import { defineComponent, ref } from 'vue';
+import TheDrawer from 'src/components/TheDrawer.vue';
 
 export default defineComponent({
   name: 'MainLayout',
-
-  components: {},
-
   setup() {
     const leftDrawerOpen = ref(false);
 
+    const toggleLeftDrawer = () => {
+      leftDrawerOpen.value = !leftDrawerOpen.value;
+    };
+
     return {
-      essentialLinks: linksList,
       leftDrawerOpen,
-      toggleLeftDrawer() {
-        leftDrawerOpen.value = !leftDrawerOpen.value;
-      },
-      expanded: ref(false),
-      lorem: 'Lorem ipsum dol',
+      toggleLeftDrawer,
     };
   },
+  components: { TheDrawer },
 });
 </script>
